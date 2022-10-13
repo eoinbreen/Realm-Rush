84d7330fb57a7cdd12c49ddb85941637f4680970
eoinbreen <e.breen185@gmail.com>
2022-10-13 17:33:29 +0100

    Made enemy move with Lerp Method

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
-
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +19,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
