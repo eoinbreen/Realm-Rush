30dfb6e55092dbb02d38a6d46eaf956bdac57973
eoinbreen <e.breen185@gmail.com>
2022-10-18 22:24:34 +0100

    Made Enemy Target the closest Enemy

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        float minDistance = Mathf.Infinity;
+        float thisDistance;
+
+        foreach (Enemy enemy in enemies)
+        {
+            thisDistance = Vector3.Distance(gameObject.transform.position, enemy.transform.position);
+            if(thisDistance < minDistance)
+            {
+                minDistance = thisDistance;
+                target = enemy.transform;
+            }
+        }
+    }
+
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(gameObject.transform.position, target.transform.position);
         weapon.LookAt(target);
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isAttacking)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isAttacking;
     }
 }
