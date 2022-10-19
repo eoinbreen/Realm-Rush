f27663edeaca6f97a894f300b5e6b1d305c499ec
eoinbreen <e.breen185@gmail.com>
2022-10-19 21:04:08 +0100

    Only create towers if bank has enough money

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] float startingBalance = 150f;
     [SerializeField] float currentBalance;
 
-    public float CurrentBal { get { return currentBalance;  } }
+    public float CurrentBalance { get { return currentBalance;  } }
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,5 +20,16 @@ public class Bank : MonoBehaviour
     public void Transaction(float amount)
     {
         currentBalance += amount;
+        
+        if(currentBalance < 0)
+        {
+            reloadScene();
+        }
+    }
+
+    void reloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
