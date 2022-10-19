8fa239445c9dd26824f8d6785c3883a8ce566486
eoinbreen <e.breen185@gmail.com>
2022-10-19 21:04:58 +0100

    Revert "Only create towers if bank has enough money"
    This reverts commit f27663edeaca6f97a894f300b5e6b1d305c499ec.

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] float startingBalance = 150f;
     [SerializeField] float currentBalance;
 
-    public float CurrentBalance { get { return currentBalance;  } }
+    public float CurrentBal { get { return currentBalance;  } }
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,16 +19,5 @@ public class Bank : MonoBehaviour
     public void Transaction(float amount)
     {
         currentBalance += amount;
-        
-        if(currentBalance < 0)
-        {
-            reloadScene();
-        }
-    }
-
-    void reloadScene()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
-    [SerializeField] Tower towerPrefab;
+    [SerializeField] GameObject towerPrefab;
 
     private void OnMouseDown()
     {
         if (isPlaceable)
         {
-            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
-            //Instantiate(towerPrefab, transform.position, Quaternion.identity);
-            isPlaceable = !isPlaced;
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
     }
 
