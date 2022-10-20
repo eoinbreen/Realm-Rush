33f1531e3e505293a7cf3c242950120de708a938
eoinbreen <e.breen185@gmail.com>
2022-10-20 20:38:09 +0100

    Added text to screen to display gold in bank

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] float startingBalance = 150f;
     [SerializeField] float currentBalance;
 
+    [SerializeField] TMP_Text bankText;
+
     public float CurrentBalance { get { return currentBalance;  } }
     // Start is called before the first frame update
     void Awake()
     {
         currentBalance = startingBalance;
+
+        bankText.text = "Gold : " + currentBalance;
     }
 
   
@@ -20,8 +25,9 @@ public class Bank : MonoBehaviour
     public void Transaction(float amount)
     {
         currentBalance += amount;
-        
-        if(currentBalance < 0)
+        bankText.text = "Gold : " + currentBalance;
+
+        if (currentBalance < 0)
         {
             reloadScene();
         }
