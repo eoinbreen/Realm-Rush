b12e7e6f42faaac5799803b97e4a529102313161
eoinbreen <e.breen185@gmail.com>
2022-11-06 13:06:48 +0000

    Blocked Tiles fom Pathfinding

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] List<Tile> path = new List<Tile>();
     
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
@@ -32,10 +32,10 @@ public class EnemyMover : MonoBehaviour
 
         foreach (Transform child in parent.transform)
         {
-            Waypoint waypoint = child.GetComponent<Waypoint>();
-            if(waypoint != null)
+            Tile tile = child.GetComponent<Tile>();
+            if(tile != null)
             {
-                path.Add(waypoint);
+                path.Add(tile);
             }
         }
     }
@@ -47,7 +47,7 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-        foreach(Waypoint waypoint in path)
+        foreach(Tile waypoint in path)
         {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
--- Assets/Environment/Tile.cs
+++ Assets/Environment/Tile.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour
+public class Tile : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
     [SerializeField] Tower towerPrefab;
 
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    private void Start()
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
     private void OnMouseDown()
     {
         if (isPlaceable)
